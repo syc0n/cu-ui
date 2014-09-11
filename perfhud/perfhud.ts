ed3315c7a191d43a09fa9fcb2bbc336b02fc89a5
Bryce Darling <bdarling@citystateentertainment.com>
2014-09-11 10:45:43 -0400

    added location and speed to perfhud

--- perfhud/perfhud.ts
+++ perfhud/perfhud.ts
@@ -16,7 +16,9 @@ module PerfHUD {
                 "tcp " + Math.round(cuAPI.netstats_tcpBytes) + " B / " + Math.round(cuAPI.netstats_tcpMessages) + " msg (" + (cuAPI.netstats_tcpBytes / cuAPI.netstats_tcpMessages).toFixed(1) + " B/msg)<br/>" +
                 "udp " + Math.round(cuAPI.netstats_udpBytes) + " B / " + Math.round(cuAPI.netstats_udpPackets) + " pkt (" + (cuAPI.netstats_udpBytes / cuAPI.netstats_udpPackets).toFixed(1) + " B/pkt)<br/>" +
                 "plNew " + (cuAPI.netstats_players_newBits / 8).toFixed(1) + " B / " + Math.round(cuAPI.netstats_players_newCount) + " E (" + (cuAPI.netstats_tcpBytes / cuAPI.netstats_players_newCount).toFixed(1) + " b/E)<br/>" +
-                "plUpd " + (cuAPI.netstats_players_updateBits / 8).toFixed(1) + " B / " + Math.round(cuAPI.netstats_players_udpateCount) + " E (" + (cuAPI.netstats_players_updateBits / cuAPI.netstats_players_udpateCount).toFixed(1) + " b/E)"
+                "plUpd " + (cuAPI.netstats_players_updateBits / 8).toFixed(1) + " B / " + Math.round(cuAPI.netstats_players_udpateCount) + " E (" + (cuAPI.netstats_players_updateBits / cuAPI.netstats_players_udpateCount).toFixed(1) + " b/E)<br/>" +
+                "position: (" + (cuAPI.locationX).toFixed(1) + ", " + (cuAPI.locationY).toFixed(1) + ", " + (cuAPI.locationZ).toFixed(1) + ")<br/>" +
+                "speed: " + (cuAPI.speed).toFixed(1)
                 );
         }
     }
